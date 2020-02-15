a00abf92e312834f0ba9e9396fd51fa4087d7c18
Patrick <prm5983@rit.edu>
2020-02-15 15:34:28 -0500

    Slightly retooled playerwarp and tagged player with player

--- Assets/Scripts/PlayerWarp.cs
+++ Assets/Scripts/PlayerWarp.cs
@@ -5,31 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class PlayerWarp : MonoBehaviour
 {
-	public enum WarpType
-	{
-		Death,
-		Win
-	}
-
-    public WarpType warpType;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            switch (warpType)
-            {
-                case WarpType.Death:
-                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-                    break;
-
-                case WarpType.Win:
-                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-                    break;
-
-                default:
-                    break;
-            }
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
